eb93f039aea4682b50b03c9e9a0c0b7298a442d5
minukheo1022@gmail.com <minukheo1022@gmail.com>
2022-02-04 00:26:44 +0900

    Review

--- src/main/java/Controller/CafeReviewController.java
+++ src/main/java/Controller/CafeReviewController.java
@@ -27,7 +27,7 @@ public class CafeReviewController {
     @NonNull
     private final CafeReviewService cafeReviewService;
 
-
+    // 사용자 정보 얻을때 principal or authentication 토큰 사용
     @PostMapping("/add/{cafe_ID}")
     public ResponseEntity<List<CafeReview>> addComment(@PathVariable Long cafe_ID,@RequestBody CafeReviewRequestDto cafereviewrequestdto, Principal principal){
         cafereviewrequestdto.setCafeNo(cafe_ID);
@@ -36,12 +36,11 @@ public class CafeReviewController {
 
         find_ID.ifPresent(findUser_ID->{
             cafereviewrequestdto.setUser_id(findUser_ID.getUser_id());
-
         });
-
         return new ResponseEntity<>(this.cafeReviewService.listComment(cafe_ID), HttpStatus.CREATED);
-
+        //api 응답 ->entitny or body 사용
     }
+
     // READ
     @GetMapping("/list/{cafe_ID}")
     public ResponseEntity<List<CafeReview>> List_Comment(@PathVariable Long cafe_ID){
